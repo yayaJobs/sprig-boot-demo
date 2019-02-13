2f04501f4bf1845962174946c6126cb2fac26314
yaya <wangzhenya@ebyf.com.cn>
2019-02-13 16:23:34 +0800

    demo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sprig-boot-demo
 java sprig-boot demo
+gradle bootRun
+
+
+export apphome=/home/cmsdev
+start(){
+ now=`date "+%Y%m%d%H%M%S"`
+ exec $JAVA ${JAVA_OPT}  -DAPP_HOME=$apphome -jar /home/cmsdev/jar/cms-web-0.0.2-SNAPSHOT.jar --spring.config.location=file:/home/cmsdev/properties/  5 > /home/cmsdev/logs/cms_web_"$now".log &
+}
--- core/src/main/java/com/yaya/spring/boot/demo/core/DemoApplication.java
+++ core/src/main/java/com/yaya/spring/boot/demo/core/DemoApplication.java
@@ -3,12 +3,12 @@ package com.yaya.spring.boot.demo.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.yaya.spring.boot.demo")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
 
